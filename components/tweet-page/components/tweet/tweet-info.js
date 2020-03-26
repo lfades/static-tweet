b00af393fa2fc45ec4cbbb0c0cb8bd6ec4be3c67
Luis Alvarez <luis@zeit.co>
2020-03-26 16:50:33 -0500

    Updated next and load the tweet date on mount

--- components/tweet-page/components/tweet/tweet-info.js
+++ components/tweet-page/components/tweet/tweet-info.js
@@ -1,10 +1,12 @@
 import format from 'date-fns/format';
 import formatNumber from '../../../../lib/format-number';
+import useMounted from '../../../../lib/use-mounted';
 
 export default function TweetInfo({ tweet }) {
+  const mounted = useMounted();
   const likeUrl = `https://twitter.com/intent/like?tweet_id=${tweet.id}`;
   const tweetUrl = `https://twitter.com/${tweet.username}/status/${tweet.id}`;
-  const createdAt = new Date(tweet.createdAt);
+  const createdAt = typeof window !== 'undefined' && mounted ? new Date(tweet.createdAt) : null;
 
   return (
     <div className="info">
@@ -14,12 +16,16 @@ export default function TweetInfo({ tweet }) {
         </div>
         {tweet.likes > 0 && <span className="likes">{formatNumber(tweet.likes)}</span>}
       </a>
-      <a className="time" href={tweetUrl} target="_blank" rel="noopener noreferrer">
-        <time title={`Time Posted: ${createdAt.toUTCString()}`} dateTime={createdAt.toISOString()}>
-          {format(createdAt, 'h:mm a - MMM d, y')}
-        </time>
-      </a>
-
+      {createdAt && (
+        <a className="time" href={tweetUrl} target="_blank" rel="noopener noreferrer">
+          <time
+            title={`Time Posted: ${createdAt.toUTCString()}`}
+            dateTime={createdAt.toISOString()}
+          >
+            {format(createdAt, 'h:mm a - MMM d, y')}
+          </time>
+        </a>
+      )}
       <style jsx>{`
         a {
           text-decoration: none;
