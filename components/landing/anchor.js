1867e6b6e24c7928e8b6dbfcb18b47eb014ec512
Luis Alvarez <luis@zeit.co>
2020-04-21 21:39:23 -0500

    Fix anchor component

--- components/landing/anchor.js
+++ components/landing/anchor.js
@@ -1,7 +1,7 @@
 import { forwardRef } from 'react';
 import styles from './anchor.module.css';
 
-const A = forwardRef(({ children, href, title, blank = true }, ref) => (
+const A = forwardRef(({ children, href, title, blank = true, onClick }, ref) => (
   <a
     ref={ref}
     href={href}
@@ -9,6 +9,7 @@ const A = forwardRef(({ children, href, title, blank = true }, ref) => (
     rel={blank ? 'noopener noreferrer' : null}
     title={title || href}
     className={styles.anchor}
+    onClick={onClick}
   >
     {blank ? <>{children}&nbsp;&raquo;</> : children}
   </a>
