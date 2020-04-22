87b581c18cafaeff7b495f80ed88f5d83055fd1d
Luis Alvarez <luis@zeit.co>
2020-04-22 11:13:34 -0500

    Added max-height

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,9 @@ export default function Index({ tweet }) {
         That means no Twitter embed <Code className="inline">`{`<iframe>`}`</Code>, no JS, no layout
         and scrolling jumps, no slowness, great SEO, great ligthouse scores:
       </P>
-      <Img src="/assets/lighthouse-score-01.png" alt="Very good lighthouse score" />
+      <div style={{ maxHeight: '215px' }}>
+        <Img src="/assets/lighthouse-score-01.png" alt="Very good lighthouse score" />
+      </div>
       <P>To see this in action, try statically rendering your very own tweet it:</P>
       <RandomTweet initialId="1250630175949086720" />
       <P>
