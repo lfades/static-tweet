a09bf7b13f41980609fbb6298411af13a27c8426
Luis Alvarez <luis@zeit.co>
2020-04-23 15:35:36 -0500

    Added rate limit logs

--- next.config.js
+++ next.config.js
@@ -3,8 +3,9 @@ require('dotenv').config();
 module.exports = {
   env: {
     TWITTER_API_TOKEN: process.env.TWITTER_API_TOKEN,
-    TWITTER_LABS_ENABLED: '1',
+    TWITTER_LABS_ENABLED: true,
     TWITTER_LABS_EXPANSIONS: 'attachments.poll_ids',
+    // Enabling this will break the app, don't.
     TWITTER_LOAD_WIDGETS: false,
   },
 };
