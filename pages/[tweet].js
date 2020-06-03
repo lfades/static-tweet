4df02b39824dd518ce0eb87d908100ded26cc1b9
Luis Alvarez <luis@vercel.com>
2020-06-02 19:49:43 -0500

    Make it great again

--- pages/[tweet].js
+++ pages/[tweet].js
@@ -25,10 +25,8 @@ export async function getStaticProps({ params }) {
     return { props: {} };
   }
 
-  const url = `https://twitter.com/_/status/${tweet}`;
-
   try {
-    const ast = await fetchTweetAst(url);
+    const ast = await fetchTweetAst(tweet);
     return { props: ast ? { ast } : {} };
   } catch (error) {
     // The Twitter API most likely died
