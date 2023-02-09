bc6ece72578bb9e1ae0d00c6130c7e8f5a17e0ae
LFades <luis@vercel.com>
2023-02-09 12:29:29 -0500

    Added in replying to component

--- components/twitter-layout/components/tweet/tweet.tsx
+++ components/twitter-layout/components/tweet/tweet.tsx
@@ -1,5 +1,6 @@
 import type { Tweet } from 'lib/twitter/api'
 import TweetHeader from './tweet-header'
+import TweetInReplyTo from './tweet-in-reply-to'
 import TweetBody from './tweet-body'
 import TweetInfo from './tweet-info'
 import TweetAction from './tweet-action'
@@ -9,6 +10,7 @@ const Tweet = ({ data: tweet }: { data: Tweet }) => (
   <div className={s.tweet}>
     <article className={s.article}>
       <TweetHeader tweet={tweet} />
+      {tweet.in_reply_to_status_id_str && <TweetInReplyTo tweet={tweet} />}
       <TweetBody tweet={tweet} />
       <TweetInfo tweet={tweet} />
       <TweetAction tweet={tweet} />
