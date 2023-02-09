1d8dadaf2606b81b9b68f51f4559cb2d59ba974c
LFades <luis@vercel.com>
2023-02-09 18:45:26 -0500

    Updated replies component

--- components/twitter-layout/components/tweet/tweet-replies.tsx
+++ components/twitter-layout/components/tweet/tweet-replies.tsx
@@ -1,39 +1,28 @@
-import cn from 'clsx'
+import type { FC } from 'react'
 import type { Tweet } from 'lib/twitter/api'
 import formatNumber from 'lib/format-number'
 import s from './tweet-replies.module.css'
 
-export default function TweetReplies({ tweet }: { tweet: Tweet }) {
+const TweetReplies: FC<{ tweet: Tweet }> = ({ tweet }) => {
   const userUrl = `https://twitter.com/${tweet.user.screen_name}`
   const tweetUrl = `${userUrl}/status/${tweet.id_str}`
-  const count = tweet.conversation_count
-  const isConversation = tweet.news_action_type === 'conversation' || count > 4
 
   return (
-    <>
-      {isConversation ? (
-        <div className={s.replies}>
-          <a
-            className={s.link}
-            href={tweetUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <span className={s.text}>Read {formatNumber(count)} replies</span>
-          </a>
-        </div>
-      ) : (
-        <a
-          className={s.link}
-          href={userUrl}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className={cn('icon', s['icon-profile'])} />
-          <span className={s.text}>See {tweet.user.name}'s other Tweets</span>
-          <div className={cn('icon', s['icon-chevron'])} />
-        </a>
-      )}
-    </>
+    <div className={s.replies}>
+      <a
+        className={s.link}
+        href={tweetUrl}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <span className={s.text}>
+          {tweet.conversation_count > 0
+            ? `Read ${formatNumber(tweet.conversation_count)} replies`
+            : 'Read more on Twitter'}
+        </span>
+      </a>
+    </div>
   )
 }
+
+export default TweetReplies
