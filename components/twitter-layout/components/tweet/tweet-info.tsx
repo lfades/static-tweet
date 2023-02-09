b6a375772c7c8c188bda29a45ac81f8c3e42a279
LFades <luis@vercel.com>
2023-02-09 14:36:29 -0500

    Updated tweet info

--- components/twitter-layout/components/tweet/tweet-info.tsx
+++ components/twitter-layout/components/tweet/tweet-info.tsx
@@ -1,4 +1,3 @@
-import cn from 'clsx'
 import format from 'date-fns/format'
 import type { Tweet } from 'lib/twitter/api'
 import useMounted from 'lib/use-mounted'
@@ -13,35 +12,32 @@ export default function TweetInfo({ tweet }: { tweet: Tweet }) {
 
   return (
     <div className={s.info}>
-      <a
-        className={s.like}
-        href={likeUrl}
-        title="Like"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <div className={s.heart}>
-          <div className={cn('icon', s['icon-heart'])} role="img" />
-        </div>
-        {tweet.favorite_count > 0 && (
-          <span className={s.likes}>{tweet.favorite_count}</span>
-        )}
-      </a>
       {createdAt && (
         <a
           className={s.time}
           href={tweetUrl}
           target="_blank"
           rel="noopener noreferrer"
+          aria-label={format(createdAt, 'h:mm a · MMM d, y')}
         >
-          <time
-            title={`Time Posted: ${createdAt.toUTCString()}`}
-            dateTime={createdAt.toISOString()}
-          >
-            {format(createdAt, 'h:mm a - MMM d, y')}
+          <time dateTime={createdAt.toISOString()}>
+            {format(createdAt, 'h:mm a · MMM d, y')}
           </time>
         </a>
       )}
+      <a
+        className={s.infoLink}
+        href="https://help.twitter.com/en/twitter-for-websites-ads-info-and-privacy"
+        title="Like"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <svg viewBox="0 0 24 24" aria-hidden="true" className={s.infoIcon}>
+          <g>
+            <path d="M13.5 8.5c0 .83-.67 1.5-1.5 1.5s-1.5-.67-1.5-1.5S11.17 7 12 7s1.5.67 1.5 1.5zM13 17v-5h-2v5h2zm-1 5.25c5.66 0 10.25-4.59 10.25-10.25S17.66 1.75 12 1.75 1.75 6.34 1.75 12 6.34 22.25 12 22.25zM20.25 12c0 4.56-3.69 8.25-8.25 8.25S3.75 16.56 3.75 12 7.44 3.75 12 3.75s8.25 3.69 8.25 8.25z"></path>
+          </g>
+        </svg>
+      </a>
     </div>
   )
 }
