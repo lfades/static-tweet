e8a1ebf973ba140eede08bc0118d98e9f8c2ee1b
LFades <luis@vercel.com>
2023-02-10 08:12:09 -0500

    Handle multiple images

--- components/twitter-layout/components/tweet/tweet-media.tsx
+++ components/twitter-layout/components/tweet/tweet-media.tsx
@@ -1,31 +1,45 @@
 import type { FC } from 'react'
 import Image from 'next/image'
+import clsx from 'clsx'
 import type { Tweet } from 'lib/twitter/api'
 import { getMediaUrl, getTweetUrl } from 'lib/twitter/utils'
 import s from './tweet-media.module.css'
 
-const TweetMedia: FC<{ tweet: Tweet }> = ({ tweet }) => (
-  <div className={s.root}>
-    {tweet.mediaDetails?.map((media) => (
-      <a
-        key={media.display_url}
-        href={getTweetUrl(tweet)}
-        className={s.media}
-        target="_blank"
-        rel="noopener noreferrer"
+const TweetMedia: FC<{ tweet: Tweet }> = ({ tweet }) => {
+  const length = tweet.mediaDetails?.length ?? 0
+
+  return (
+    <div className={s.root}>
+      <div className={s.skeleton} />
+      <div
+        className={clsx(
+          s.mediaWrapper,
+          length > 1 && s.grid2Columns,
+          length === 3 && s.grid3,
+          length > 4 && s.grid2x2
+        )}
       >
-        <div className={s.skeleton}></div>
-        <Image
-          src={getMediaUrl(media, 'small')}
-          className={s.image}
-          alt="Image"
-          fill
-          draggable
-          unoptimized
-        />
-      </a>
-    ))}
-  </div>
-)
+        {tweet.mediaDetails?.map((media) => (
+          <a
+            key={media.media_url_https}
+            href={getTweetUrl(tweet)}
+            className={s.mediaLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src={getMediaUrl(media, 'small')}
+              className={s.image}
+              alt="Image"
+              fill
+              draggable
+              unoptimized
+            />
+          </a>
+        ))}
+      </div>
+    </div>
+  )
+}
 
 export default TweetMedia
