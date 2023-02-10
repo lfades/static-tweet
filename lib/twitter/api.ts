1b23cf7e2a53b6f04efc2b4c9b2b5f15cd4933fd
LFades <luis@vercel.com>
2023-02-10 16:30:52 -0500

    Removed markdown and html stuff

--- lib/twitter/api.ts
+++ lib/twitter/api.ts
@@ -274,43 +274,6 @@ export async function fetchUserStatus(tweetId: string) {
   throw new Error(`Fetch to the Twitter API failed with code: ${res.status}`)
 }
 
-export async function fetchTweetWithPoll(tweetId: string) {
-  const expansions = 'attachments.poll_ids'
-
-  // If there isn't an API token or Twitter Labs is not enabled, don't do anything,
-  // this is only required for Polls.
-  if (!process.env.TWITTER_API_TOKEN || !twitterLabsEnabled(expansions)) return
-
-  const res = await fetch(
-    `${API_URL}/labs/1/tweets?format=compact&expansions=${expansions}&ids=${tweetId}`,
-    {
-      headers: {
-        authorization: `Bearer ${process.env.TWITTER_API_TOKEN}`,
-      },
-    }
-  )
-
-  console.log(
-    'Twitter Labs x-rate-limit-limit:',
-    res.headers.get('x-rate-limit-limit')
-  )
-  console.log(
-    'Twitter Labs x-rate-limit-remaining:',
-    res.headers.get('x-rate-limit-remaining')
-  )
-  console.log(
-    'Twitter Labs x-rate-limit-reset:',
-    res.headers.get('x-rate-limit-reset')
-  )
-
-  if (res.ok) return res.json()
-  if (res.status === 404) return
-
-  throw new Error(
-    `Fetch to the Twitter Labs API failed with code: ${res.status}`
-  )
-}
-
 export async function getEmbeddedTweetHtml(url) {
   const res = await fetch(`https://publish.twitter.com/oembed?url=${url}`)
 
